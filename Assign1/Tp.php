dfd49da7869e9b198e7930f33038b6ef8932c657
Jeet Pandya <jeetpandya2006@gmail.com>
2026-01-19 06:16:11 +0000

    Completed

---
+++ Assign1/Tp.php
@@ -0,0 +1,16 @@
+<?php
+$php = "a";
+$lang = "php";
+$World = "lang";
+$Hello = "World";
+$a = "Hello";
+echo $a= $a;
+echo "\n";
+echo $$a= $$a;
+echo "\n";
+echo $$$a= $$$a;
+echo "\n";
+echo $$$$a= $$$$a;
+echo "\n";
+echo $$$$$a= $$$$$a;
+?>
