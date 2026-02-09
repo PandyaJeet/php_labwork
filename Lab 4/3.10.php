4365ff83f84c6cbb9e3554302114ffe87a4d9644
Jeet Pandya <jeetpandya2006@gmail.com>
2026-02-09 04:52:31 +0000

    3....

---
+++ Lab 4/3.10.php
@@ -0,0 +1,59 @@
+<?php
+    //sum,pow,prime,max,min,factors,factorial,space,sort_array,series
+    function main(){
+        $a = 17;
+        $b = 4;
+        $arr = [90,80,70,60,50];
+        print("Value of A : " . $a);
+        print("<br>Value of B : " . $b);
+        print("<br>Sum of A & B : " . sum($a,$b));
+        print("<br>Power of A & B : " . notpow($a,$b));
+        prime($a);
+        print("<br>Factorial of B : ". factorial($b));
+        sort_array($arr);
+        
+    }
+    function sort_array ($arr){
+        $t=0;
+        $n = sizeof($arr);
+        print("<br>Before Sort : ");
+        print_r($arr);
+        for($i = 0 ; $i < $n;$i++){
+           $swapped = False; 
+            for ($j = 0; $j < $n - $i - 1; $j++){
+                if ($arr[$j] > $arr[$j+1]){
+                    $t = $arr[$j];
+                    $arr[$j] = $arr[$j+1];
+                    $arr[$j+1] = $t;
+                    $swapped = True;
+                }
+            }
+            if ($swapped == False) break;
+        }
+        print("<br>After Sort : ");
+        print_r($arr);
+    }
+    function sum($a,$b){
+        return $a+$b;
+    }
+    function notpow($a,$b){
+        return $a**$b;
+    }
+    function prime ($a){
+        for($i = 2; $i <$a;$i++){
+            if($a%$i == 0){
+                print("<br>".$a . " is not a prime number");
+                return;
+            }
+        }
+        print("<br>".$a . " is a prime number");
+    }
+    function factorial($a){
+        $temp = 1;
+        for ($i = 1; $i <= $a ; $i++){
+            $temp = $temp * $i;
+        }
+        return $temp;
+    }
+    main();
+?>
--- Lab 4/3.7.php
+++ Lab 4/3.7.php
@@ -1,3 +1,4 @@
 <?php
-
+    $a = [1, 2, 2, 3, 4, 4, 5];
+    print_r(array_unique($a));
 ?>
