f4f14f9be95dc9036d59538440c462b9a96c6502
Jeet Pandya <jeetpandya2006@gmail.com>
2026-03-16 04:18:21 +0000

    Add initial HTML form for user input

---
+++ Lab idk/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <center>
+        <form method="POST">
+            Enter Name : <input type="text" name="tname"> <br><br>
+            Enter City : <input type="text" name="tcity"><br><br>
+            <input type="submit" name="bsubmit" value="Save">
+        </form>
+    </center>
+</body>
+</html>
